bf350837023e2eab99dcdb824e4cf6228c1c6233
Sergio Mendoza <smendoza787@gmail.com>
2019-09-29 19:16:07 -0600

    add styled gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
     'gatsby-plugin-offline',
     `gatsby-plugin-typescript`,
     'gatsby-transformer-remark',
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-contentful`,
       options: contentfulConfig,
