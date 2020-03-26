010f582d63c98477047ee91c413206cafc56e64a
Sergio Mendoza <smendoza787@gmail.com>
2020-03-26 12:39:42 -0600

    yowza

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Layout from '../components/layout'
 import PortfolioBlock from '../components/PortfolioBlock'
+import styled from 'styled-components'
+
+const ComingSoonText = styled.h1`
+  font-family: 'Pacifico', sans-serif;
+  text-align: center;
+  margin: 1.5rem 0;
+`
 
 function PortfolioPage() {
   return (
     <Layout>
-      <h1>🚧Coming Soon 🚧</h1>
+      <ComingSoonText>🚧 Coming Soon 🚧</ComingSoonText>
     </Layout>
   )
 }
